d1e1c97689e1e7fbb6a3ad0d242c178be94c7191
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 <dannykill4221x@gmail.com>
2023-03-03 12:49:58 -0300

    question 1072 modified

--- Beecrowd/Question1072/bee1072.js
+++ Beecrowd/Question1072/bee1072.js
@@ -1,15 +1,15 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
 var lines = input.split('\n');
 
-lines = lines.map(value => parseFloat(value));
+let X = lines.shift();
 
-lines.shift();
+lines = lines.map(value => parseInt(value));
 
 let countIn = 0;
 let countOut = 0;
 
-for(let value of lines) {
-    if(value >= 10 && value <= 20)
+for(let i = 0; i < X; i++) {
+    if(lines[i] >= 10 && lines[i] < 20)
         countIn++;
     else 
         countOut++;
